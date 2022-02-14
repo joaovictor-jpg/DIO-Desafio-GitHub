3f0a6d0aaaa2b6b2a022ab21a59cd956ab771471
João Victor <joaovictormdasilva676@gmail.com>
2022-02-14 18:40:03 -0300

    Instalando e configurando os principais Ambientes de trabalho

---
+++ Dominando IDEs Java/primeiro programa/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		int a = 2;
+		int b = 3;
+		System.out.println("Heloo World! " + (a+b));
+		
+		System.out.println(gato);
+	}
+}
